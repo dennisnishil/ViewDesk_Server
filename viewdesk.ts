a6a33bbfeb2a28d225cc6872b084dd2388812074
dennisnishil <dennisnishil@gmail.com>
2026-09-11 13:28:32 +0400

    Update viewdesk.ts

--- viewdesk.ts
+++ viewdesk.ts
@@ -373,7 +373,18 @@ app.get("/", (_req: Request, res: Response) => {
     const rtcConfig = {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        // Free TURN relay server for routing streams through strict firewalls
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelay",
+          credential: "openrelay"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelay",
+          credential: "openrelay"
+        }
       ]
     };
 
